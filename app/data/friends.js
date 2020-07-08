aa4dfc006812c23137a064dc8af7ce6ac4200934
Jarrett Dougherty <jarrettdougherty@gmail.com>
2020-07-07 23:53:53 -0400

    modal is working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,47 +1,66 @@
 var friends = [
     {
-        name:"Tim",
+        name:"Michael Scott",
+        photo: "https://vignette.wikia.nocookie.net/theoffice/images/b/be/Character_-_MichaelScott.PNG/revision/latest/top-crop/width/360/height/360?cb=20200413224550",
         scores:[
             5,
-            4,
-            3,
-            2,
+            5,
             1,
+            2,
+            2,
+            2,
             5,
             4,
+            5,
+            1
+        ]
+    },
+    {
+        name:"Dwight Schrute",
+        photo: "https://cdn.pastemagazine.com/www/articles/2020/05/20/the-office-dwight-schrute-main.jpg",
+        scores:[
+            4,
+            1,
             3,
+            1,
+            5,
+            5,
+            5,
+            5,
+            2,
+            5
+        ]
+    },
+    {
+        name:"Jim Halpert",
+        photo: "https://vignette.wikia.nocookie.net/theoffice/images/e/e9/Character_-_JimHalpert.PNG/revision/latest/top-crop/width/360/height/360?cb=20200414162003",
+        scores:[
+            3,
+            3,
+            4,
+            3,
+            1,
+            3,
+            3,
+            2,
             2,
             1
         ]
     },
     {
-        name:"Jim",
-        scores:[
-            1,
-            2,
-            3,
-            4,
-            5,
-            1,
-            2,
-            3,
-            4,
-            5
-        ]
-    },
-    {
-        name:"Michael",
+        name:"Angela Martin",
+        photo: "https://i.pinimg.com/originals/e5/67/cc/e567cc9331491412a1e53e83c7766a1a.jpg",
         scores:[
             3,
-            4,
-            5,
-            1,
             2,
             3,
-            4,
             5,
+            5,
+            4,
+            4,
             1,
-            2
+            5,
+            3
         ]
     }
 ];
